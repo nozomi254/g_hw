22e1d4af07578b80ad4eb805abd4243b791993ad
nozomi254 <n.nozomi523@gmail.com>
2019-06-07 15:58:31 +0900

    up

---
+++ g_hw_2.py
@@ -0,0 +1,53 @@
+import numpy, sys, time
+
+if (len(sys.argv) != 2):
+    print("usage: python %s N" % sys.argv[0])
+    quit()
+# 入力した引数の個数？が２じゃないとダメなんか?
+# 使い方？引数の１番目　Nって何急に
+
+n = int(sys.argv[1])
+a = numpy.zeros((n, n)) # Matrix A
+b = numpy.zeros((n, n)) # Matrix B
+c = numpy.zeros((n, n)) # Matrix C
+# 0で初期化した配列を作っているぽい　3つ
+# 入力した引数の２番目を行と列のでかさに？してる
+
+# Initialize the matrices to some values.
+for i in range(n):
+    for j in range(n):
+        a[i, j] = i * n + j
+        b[i, j] = j * n + i
+        c[i, j] = 0
+# a 012 b 036
+#   345   147
+#   678   258
+# の行列ができるっぽい　なんで、、、
+#c 5  14  23
+# 14  50  86
+# 23  86 149
+begin = time.time()
+
+######################################################
+# Write code to calculate C = A * B                  #
+# (without using numpy librarlies e.g., numpy.dot()) #
+######################################################
+for p in range(n):
+    for q in range(n):
+        for i in range(n):
+
+            c[p, q] += a[p, i] * b[i, q]
+
+
+end = time.time()
+print("time: %.6f sec" % (end - begin))
+
+# Print C for debugging. Comment out the print before measuring the execution time.
+total = 0
+for i in range(n):
+    for j in range(n):
+        # print c[i, j]
+        total += c[i, j]
+# Print out the sum of all values in C.
+# This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
+print("sum: %.6f" % total)
